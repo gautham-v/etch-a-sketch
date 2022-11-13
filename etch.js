0cd00e62df72cd8b013448bdb352edbc89bd7eab
gautham-v <gautham.vemulapalli@gmail.com>
2022-11-13 11:33:46 -0500

    random colors, darkened colors and cleanup

--- etch.js
+++ etch.js
@@ -1,6 +1,42 @@
 // Create a 16x16 grid of square divs
 function color(e){
-    this.setAttribute('style', 'background-color: #A6AAE4');
+    let startingColor = window.getComputedStyle(this).getPropertyValue("background-color"); 
+    let red = startingColor.substring(4,startingColor.indexOf(','));
+    let green = startingColor.substring(startingColor.indexOf(' ')+1,startingColor.lastIndexOf(','));
+    let blue = startingColor.substring(startingColor.lastIndexOf(' ')+1,startingColor.lastIndexOf(')'));
+
+    if (startingColor === 'rgb(255, 255, 255)'){
+        const randomColor = randomColorGenerator();
+        this.setAttribute('style', `background-color: ${randomColor}`);
+        this.redReductionValue = getDarkerValue(red);
+        this.greenReductionValue = getDarkerValue(green);
+        this.blueReductionValue = getDarkerValue(blue);
+    }
+    else {
+        red = Math.max(0, red - this.redReductionValue);
+        green = Math.max(0, green - this.greenReductionValue);
+        blue = Math.max(0, blue - this.blueReductionValue);
+        let darkerColor = `rgb(${red}, ${green}, ${blue})`;
+        this.setAttribute('style', `background-color: ${darkerColor}`);
+    }
+}
+
+function getDarkerValue(color){
+    let reduction = Math.ceil(color/10);
+    return reduction;
+}
+
+function randomColorGenerator(){
+    let min = 0;
+    let max = 256;
+    let randomColor = 'rgb(';
+    for (i = 0; i<=2; i++){
+        var val = Math.floor(Math.random() * (max - min + 1) + min);
+        randomColor += val + ',';
+    }
+    randomColor = randomColor.slice(0,-1);
+    randomColor += ')'
+    return randomColor;
 }
 
 function createGrid (columns){
@@ -10,7 +46,6 @@ function createGrid (columns){
         const div = document.createElement('div');
         div.classList.add('grid-item');
         container.appendChild(div);
-        console.log(`div parent element: ${div.parentElement.id}`);
     }
     for (let i = 1; i<=columns-1; i++){
         numOfColumns += ' auto';
@@ -45,7 +80,6 @@ function resizeGrid(){
 
 const container = document.querySelector('#container');
 createGrid(16);
-
 const resizeButton = document.querySelector('.resize-btn');
 resizeButton.addEventListener('click', resizeGrid);
 
